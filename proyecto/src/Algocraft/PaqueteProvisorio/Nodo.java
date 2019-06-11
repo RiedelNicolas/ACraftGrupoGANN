97092846e5945fa67a4396e3dd0c24f755c37842
Nicolas Riedel <nariedel99@gmail.com>
2019-06-11 15:11:23 -0300

    agrego la implementacion de nodo

--- proyecto/src/Algocraft/PaqueteProvisorio/Nodo.java
+++ proyecto/src/Algocraft/PaqueteProvisorio/Nodo.java
@@ -1,4 +1,29 @@
 package Algocraft.PaqueteProvisorio;
 
+
+import Algocraft.Excepciones.NodoOcupadoException;
+
 public class Nodo {
+    private Posicionable contenido;
+
+    public Posicionable getContenido() {
+        return contenido;
+    }
+
+    public void setContenido(Posicionable contenidoRecibido) throws NodoOcupadoException {
+        if( this.ocupado() ){
+            throw new NodoOcupadoException();
+        }
+        contenido=contenidoRecibido;
+    }
+
+
+    public void limpiar(){
+        this.contenido = null;
+    }
+
+    public boolean ocupado(){
+        return (contenido != null);
+    }
+
 }
