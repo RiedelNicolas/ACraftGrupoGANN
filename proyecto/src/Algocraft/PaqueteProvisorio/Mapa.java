9503c2fcd0a346a9e5cebe779d1f6228ad1569a1
Nicolas Riedel <nariedel99@gmail.com>
2019-06-11 21:34:57 -0300

    agrego implemento tablero, intento arreglar repo

--- proyecto/src/Algocraft/PaqueteProvisorio/Mapa.java
+++ proyecto/src/Algocraft/PaqueteProvisorio/Mapa.java
@@ -1,17 +1,65 @@
 
 package Algocraft.PaqueteProvisorio;
 
+import Algocraft.Excepciones.PosicionFueraDeRangoException;
+
 public class Mapa {
     private Nodo[][] plano;
+    private int alto;
+    private int ancho;
 
-    public Mapa(int ancho, int alto){
+    //para el singleton
+    private static Mapa instancia_unica = null;
+
+    public static Mapa NuevoConDimesiones(int ancho, int alto){
+        if(instancia_unica == null ){
+            instancia_unica = new Mapa(ancho,alto);
+        }
+
+        return instancia_unica;
+    }
+
+    private Mapa(int ancho, int alto){
         plano = new Nodo[ancho][alto];
+        this.alto = alto;
+        this.ancho = ancho;
     }
 
-    public void limpiar( Posicion posicion) {
-
-        ( plano[posicion.getVertical()][posicion.getHorizontal()] ).limpiar();
+    public void agregarElemento(Posicionable elemento, Posicion pos){
+        if(fueraDeRango(pos)){
+            throw new PosicionFueraDeRangoException();
+        }
+        Nodo nodoActual = plano[pos.getHorizontal()][pos.getVertical()];
+        nodoActual.setContenido(elemento);
     }
 
-    public boolean fueraDeRango(Posicion posicion){ return false;}
+    public Posicionable getElemento(Posicion pos){
+        if( fueraDeRango(pos) ){
+            throw new PosicionFueraDeRangoException();
+        }
+
+        Nodo nodoActual = plano[pos.getHorizontal()][pos.getVertical()];
+        return nodoActual.getContenido();
+    }
+
+    public void limpiar(Posicion posicion) {
+
+        ( plano[posicion.getHorizontal()][posicion.getVertical()] ).limpiar();
+    }
+
+    public void mover(Posicion posicionAnterior, Posicion posicionNueva){
+        if( fueraDeRango(posicionAnterior) || fueraDeRango(posicionNueva) ){
+            throw new PosicionFueraDeRangoException();
+        }
+        //esto tiene que quedar mas lindo.
+        Nodo auxiliar = plano[posicionAnterior.getHorizontal()][posicionAnterior.getVertical()];
+        plano[posicionNueva.getHorizontal()][posicionNueva.getVertical()] = auxiliar;
+        limpiar(posicionAnterior);
+
+    }
+
+    public boolean fueraDeRango(Posicion posicion){
+            return( !(posicion.getHorizontal()<=ancho && posicion.getVertical()<=alto) );
+        }
+
 }
